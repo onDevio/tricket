a0b4db3689bdb2746175eae73dafcbe40c3f3ac7
ondevio <info@ondevio.com>
2015-12-15 10:41:40 +0000

    Issue #1, mailin webhook problems

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,17 @@ router.get('/clients', function(req, res, next) {
   res.render('dashboard/clients.html');
 });
 
+router.post('/emails', function(req, res, next) {
+  console.log('POST /emails request.body:' + JSON.stringify(req.body));
+  res.status(200).end();
+});
+
+router.head('/emails', function(req, res, next) {
+  res.status(200).end();
+});
+
+router.get('/emails', function(req, res, next) {
+  res.status(200).end();
+});
+
 module.exports = router;
