caa271f2cb85c058cba33ef0325205a0aec1aefd
zephyrante <lmunoz@ondevio.com>
2015-12-15 17:39:50 +0100

    Issue #11, Alta de ticket arranque de la vista

--- routes/private.js
+++ routes/private.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
-router.get('/tickets', function(req, res, next) {
+router.get('/tickets', function(req, res) {
   res.render('dashboard/tickets.html');
 });
 
-router.get('/clients', function(req, res, next) {
+router.get('/clients', function(req, res) {
   res.render('dashboard/clients.html');
+});
+
+router.get('/ticket/new', function(req, res) {
+  res.render('dashboard/ticket.html');
+});
+
+router.post('/ticket/new', function(req, res) {
+  console.log(req.body);
+  res.render('dashboard/tickets.html');
 });
 
 module.exports = router;
