024e15aaa8826b98802ab5a5e61664f76c4148e6
zephyrante <lmunoz@ondevio.com>
2015-12-18 12:20:27 +0100

    Issue #25, vista de reportes

---
+++ plugins/render/cms-render-report-result.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var log = require('debug')('temply:cms-render-report-result');
+
+module.exports = function(data, $element, callback) {
+  if (!data) {
+    callback();
+    return;
+  }
+  
+  var tickets = data.contents;
+  //log(JSON.stringify(tickets));
+  var totalWork = 0;
+  tickets.forEach(function(ticket){
+  	//log(ticket);
+    var notes = ticket.notes;
+    var work = 0;
+    notes.forEach(function(note){
+      //log(note);
+      work += parseInt(note.worklog);
+      totalWork += work;
+    });  	
+    
+  	$element.find('tbody').append('<tr class="clickable-row" data-href="/app/ticket/'+ticket.ticket_id+'"><td>'+ticket.ticket_id+' </td><td>'+ticket.title+' </td><td>'+ticket.status+' </td><td align="right">'+work+' </td></tr>');
+  });
+  //log(totalWork);
+  //TODO: Clientes
+  $element.find('.client').text(totalWork.toString());
+  $element.find('.total').text("Total work log: "+ totalWork.toString());
+
+  callback(data);
+
+}
