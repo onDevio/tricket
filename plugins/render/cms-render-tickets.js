4668efb0c534f17eaae73279c9c1d90ce187758d
mefernandez <mariano.fernandez@gmail.com>
2017-07-05 10:39:49 +0200

    Closes #34, muestro ticket.worklog o la suma de notes.worklog en su defecto

--- plugins/render/cms-render-tickets.js
+++ plugins/render/cms-render-tickets.js
@@ -18,15 +18,18 @@ module.exports = function(data, $element, callback) {
   	var date = new Date(ticket.dateCreated);
     var notes = ticket.notes;
     var asignee = ticket.asignee || 'Not asigned';
-    var work = 0;
-    notes.forEach(function(note){
-      //log(note);
-      work += parseInt(note.worklog);
-    });
+    var worklog = ticket.worklog;
+    if (!worklog) {
+      var worklog = 0;
+      notes.forEach(function(note){
+        //log(note);
+        worklog += parseInt(note.worklog);
+      });
+    }
     var trash = ticket.status === 'Trashed' ? '<a href="/api/ticket/'+ticket.ticket_id+'/untrash" class="btn btn-default"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Untrash</a></td>' : '<a href="/api/ticket/'+ticket.ticket_id+'/trash" class="btn btn-default"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Trash</a></td>';
 
     if(ticket.asignee === currentUser.displayName){
-      $element.find('#mine tbody').append('<tr class="clickable-row" data-href="/app/ticket/'+ticket.ticket_id+'"><td>'+ticket.ticket_id+' </td><td>'+ticket.title+' </td><td>'+moment(date).fromNow()+' </td><td>'+ticket.status+' </td><td>'+asignee+' </td><td>'+work+' </td><td>'+ticket.customer.email+' </td><td>'+trash+'</td></tr>');
+      $element.find('#mine tbody').append('<tr class="clickable-row" data-href="/app/ticket/'+ticket.ticket_id+'"><td>'+ticket.ticket_id+' </td><td>'+ticket.title+' </td><td>'+moment(date).fromNow()+' </td><td>'+ticket.status+' </td><td>'+asignee+' </td><td>'+worklog+' </td><td>'+ticket.customer.email+' </td><td>'+trash+'</td></tr>');
     }
 
     var statusLowerCase = ticket.status.toLowerCase();
@@ -36,7 +39,7 @@ module.exports = function(data, $element, callback) {
     }
   	$element
       .find('#'+statusLowerCase+' tbody')
-      .append('<tr class="clickable-row" data-href="/app/ticket/'+ticket.ticket_id+'"><td>'+ticket.ticket_id+' </td><td>'+ticket.title+' </td><td>'+moment(date).fromNow()+' </td><td>'+ticket.status+' </td><td>'+asignee+' </td><td>'+work+' </td><td>'+ticket.customer.email+' </td><td>'+trash+'</td></tr>');
+      .append('<tr class="clickable-row" data-href="/app/ticket/'+ticket.ticket_id+'"><td>'+ticket.ticket_id+' </td><td>'+ticket.title+' </td><td>'+moment(date).fromNow()+' </td><td>'+ticket.status+' </td><td>'+asignee+' </td><td>'+worklog+' </td><td>'+ticket.customer.email+' </td><td>'+trash+'</td></tr>');
   });
 
   callback(data);
